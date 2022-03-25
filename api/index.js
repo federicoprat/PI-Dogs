923f380e125cbdb34151ea3d9c5574af1aa4a861
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-03-24 22:44:39 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -21,7 +21,7 @@ require('dotenv').config();
 const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 
-const {PORT} = process.env
+const PORT = process.env.PORT
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
