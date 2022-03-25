4a6ab65cb140917ef9c6c60b15e10ede9d362aa5
federicoprat <82243049+federicoprat@users.noreply.github.com>
2022-03-24 23:13:56 -0300

    Update index.js

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -11,7 +11,12 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
+router.get("/dogs/:id", dogById);
+router.get("/dogs", dogsHome);
+router.get("/temperament", temperaments)
+router.post("/dog", createDog)
+router.get("/dogimage", sendImage)
+router.get("/", (req, res) => res.json({data: "cannot GET /"}))
 //https://cdn2.thedogapi.com/images/BJa4kxc4X.jpg
 module.exports = router;
 
